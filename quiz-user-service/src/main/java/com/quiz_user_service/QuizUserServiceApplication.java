61bdcb659a011aae39d0ea6cf13d4cadb1e63430
krokodil1113 <krokodil111384@gmail.com>
2024-10-28 12:42:21 +0100

    Initial commit of quiz-user-service

---
+++ quiz-user-service/src/main/java/com/quiz_user_service/QuizUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.quiz_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizUserServiceApplication.class, args);
+	}
+
+}
---
+++ quiz-user-service/src/test/java/com/quiz_user_service/QuizUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
